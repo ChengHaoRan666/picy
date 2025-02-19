605e952eb086eb16fa60c9e2d2cdf199eb336585
ChengHaoRan <2640708884@qq.com>
2025-02-19 13:32:32 +0800

    图床项目创建

---
+++ src/main/java/com/chr/picy/PicyApplication.java
@@ -0,0 +1,16 @@
+package com.chr.picy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class PicyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicyApplication.class, args);
+        log.info("service...");
+    }
+
+}
